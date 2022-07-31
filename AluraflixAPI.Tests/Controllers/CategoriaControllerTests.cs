ded3d550e217d6d479c4d81fd9afff866ebbedf4
Fred Davin <fredericodavin@gmail.com>
2022-07-31 14:48:49 -0300

    Realizando últimos testes unitários

---
+++ AluraflixAPI.Tests/Controllers/CategoriaControllerTests.cs
@@ -0,0 +1,216 @@
+﻿using AluraflixAPI.Controllers;
+using AluraflixAPI.Services;
+using AluraflixAPI.ViewModels;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AluraflixAPI.Tests.Controllers
+{
+    public class CategoriaControllerTests
+    {
+        private readonly Mock<ICategoriaService> _mockService;
+        private readonly CategoriasController _controller;
+
+        public CategoriaControllerTests()
+        {
+            _mockService = new Mock<ICategoriaService>();
+            _controller = new CategoriasController(_mockService.Object);
+        }
+
+        [Fact]
+        public void CadastrarCategoria_ComInformacoesValidas_ResultadoCreatedAtActionComRetorno()
+        {
+            // Arrange
+            CreateCategoriaViewModel categoriaParaCadastrar = new CreateCategoriaViewModel
+            {
+                Titulo = "CORRIDA",
+                Cor = "AMARELO"
+            };
+
+            ReadCategoriaViewModel categoriaCadastrada = new ReadCategoriaViewModel
+            {
+                Id = 5,
+                Titulo = "CORRIDA",
+                Cor = "AMARELO"
+            };
+
+            _mockService.Setup(s => s
+                .CadastrarCategoria(categoriaParaCadastrar))
+                .Returns(categoriaCadastrada);
+
+            // Act
+            var actionResult = _controller.CadastrarCategoria(categoriaParaCadastrar);
+            var objectResult = actionResult as CreatedAtActionResult;
+            var categoriaResult = objectResult.Value as ReadCategoriaViewModel;
+
+            // Assert
+            Assert.NotNull(categoriaResult);
+            Assert.Equal(201, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void ConsultarCategoriaPorId_ComIdValido_ResultadoOkComRetorno()
+        {
+            // Arrange
+            ReadCategoriaViewModel categoriaConsultada = new ReadCategoriaViewModel
+            {
+                Id = 5,
+                Titulo = "CORRIDA",
+                Cor = "AMARELO"
+            };
+
+            _mockService.Setup(s => s
+                .ConsultarCategoriaPorId(5))
+                .Returns(categoriaConsultada);
+
+            // Act
+            var actionResult = _controller.ConsultarCategoriaPorId(5);
+            var objectResult = actionResult as OkObjectResult;
+            var categoriaResult = objectResult.Value as ReadCategoriaViewModel;
+
+            // Assert
+            Assert.NotNull(categoriaResult);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void ConsultarCategoriaPorId_ComIdInvalido_ResultadoNotFoundSemRetorno()
+        {
+            // Arrange
+            var categoriaConsultada = new ReadCategoriaViewModel
+            {
+                Id = 5,
+                Titulo = "CORRIDA",
+                Cor = "AMARELO"
+            };
+
+            _mockService.Setup(s => s
+                .ConsultarCategoriaPorId(5))
+                .Returns(categoriaConsultada);
+
+            // Act
+            var actionResult = _controller.ConsultarCategoriaPorId(10);
+            var objectResult = actionResult as NotFoundObjectResult;
+
+            // Assert
+            Assert.Equal(404, objectResult.StatusCode);
+            Assert.Equal("Categoria não encontrada.", objectResult.Value);
+        }
+
+        [Fact]
+        public void ConsultarFilmesPorCategoria_ComIdValido_ResultadoOkComRetorno()
+        {
+            // Arrange
+            ReadVideosPorCategoriaViewModel categoriaConsultada = new ReadVideosPorCategoriaViewModel
+            {
+                Id = 5,
+                Titulo = "CORRIDA",
+                Cor = "AMARELO",
+                Videos = new List<object>()
+            };
+
+            _mockService.Setup(s => s
+                .ConsultarFilmesPorCategoriaId(5))
+                .Returns(categoriaConsultada);
+
+            // Act
+            var actionResult = _controller.ConsultarFilmesPorCategoria(5);
+            var objectResult = actionResult as OkObjectResult;
+            var categoriaResult = objectResult.Value as ReadVideosPorCategoriaViewModel;
+
+            // Assert
+            Assert.NotNull(categoriaResult);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void ConsultarCategorias_NaoPaginado_ResultadoOkComRetorno()
+        {
+            // Arrange
+            List<ReadCategoriaViewModel> colecaoDeCategorias = new List<ReadCategoriaViewModel> 
+            {
+                new ReadCategoriaViewModel
+                {
+                    Id = 5,
+                    Titulo = "CORRIDA",
+                    Cor = "AMARELO"
+                },
+                new ReadCategoriaViewModel
+                {
+                    Id = 6,
+                    Titulo = "MÚSICA",
+                    Cor = "VERDE"
+                }
+            };
+
+            _mockService.Setup(s => s
+                .ConsultarCategorias(0))
+                .Returns(colecaoDeCategorias);
+
+            // Act
+            var actionResult = _controller.ConsultarCategorias(0);
+            var objectResult = actionResult as OkObjectResult;
+            var categoriaResult = objectResult.Value as List<ReadCategoriaViewModel>;
+
+            // Assert
+            Assert.NotNull(categoriaResult);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void RemoverCategoriaPorId_ComIdValido_ResultadoOkSemRetorno()
+        {
+            // Arrange
+            _mockService.Setup(s => s
+                .RemoverCategoriaPorId(1))
+                .Returns(Result.Ok);
+
+            // Act
+            var actionResult = _controller.RemoverCategoriaPorId(1);
+            var objectResult = actionResult as OkObjectResult;
+
+            // Assert
+            Assert.Equal("Categoria deletada com sucesso.", objectResult.Value);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void AtualizarCategoriaPorId_ComIdValido_ResultadoOkComRetorno()
+        {
+            // Arrange
+            CreateCategoriaViewModel categoriaComDadosNovos = new CreateCategoriaViewModel
+            {
+                Titulo = "CORRIDA",
+                Cor = "AMARELO"
+            };
+
+            ReadCategoriaViewModel categoriaAtualizada = new ReadCategoriaViewModel
+            {
+                Id = 5,
+                Titulo = "CORRIDA",
+                Cor = "BRANCO"
+            };
+
+            _mockService.Setup(s => s
+                .AtualizarCategoriaPorId(5, categoriaComDadosNovos))
+                .Returns(categoriaAtualizada);
+
+            // Act
+            var actionResult = _controller.AtualizarCategoriaPorId(5, categoriaComDadosNovos);
+            var objectResult = actionResult as OkObjectResult;
+            var categoriaResult = objectResult.Value as ReadCategoriaViewModel;
+
+            // Assert
+            Assert.NotNull(categoriaResult);
+            Assert.Equal(200, objectResult.StatusCode);
+
+        }   
+    }
+}
--- AluraflixAPI.Tests/Controllers/VideosControllerTests.cs
+++ AluraflixAPI.Tests/Controllers/VideosControllerTests.cs
@@ -13,8 +13,17 @@ namespace AluraflixAPI.Tests.Controllers
 {
     public class VideosControllerTests
     {
+        private readonly Mock<IVideoService> _mockService;
+        private readonly VideosController _controller;
+
+        public VideosControllerTests()
+        {
+            _mockService = new Mock<IVideoService>();
+            _controller = new VideosController(_mockService.Object);
+        }
+
         [Fact]
-        public void CadastrarVideo_ComRetornoValido_ResultadoCreatedAtActionComRetorno()
+        public void CadastrarVideo_ComInformacoesValidas_ResultadoCreatedAtActionComRetorno()
         {
             // Arrange
             var videoParaCadastrar = new CreateVideoViewModel
@@ -34,13 +43,13 @@ namespace AluraflixAPI.Tests.Controllers
                 ".NET 5 innovations and Visual Studio Code. The API will be written in C#.",
                 Url = "https://www.youtube.com/watch?v=ZXdFisA_hOY"
             };
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.CadastrarVideo(videoParaCadastrar))
+
+            _mockService.Setup(s => s
+                .CadastrarVideo(videoParaCadastrar))
                 .Returns(videoCadastrado);
 
             // Act
-            var actionResult = controller.CadastrarVideo(videoParaCadastrar);
+            var actionResult = _controller.CadastrarVideo(videoParaCadastrar);
             var objectResult = actionResult as CreatedAtActionResult;
             var videoResult = objectResult.Value as ReadVideoViewModel;
 
@@ -62,13 +71,11 @@ namespace AluraflixAPI.Tests.Controllers
                 Url = "https://www.youtube.com/watch?v=ZXdFisA_hOY"
             };
 
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.ConsultarVideoPorId(1))
+            _mockService.Setup(s => s.ConsultarVideoPorId(1))
                 .Returns(videoConsultado);
 
             // Act
-            var actionResult = controller.ConsultarVideoPorId(1);
+            var actionResult = _controller.ConsultarVideoPorId(1);
             var objectResult = actionResult as OkObjectResult;
             var videoResult = objectResult.Value as ReadVideoViewModel;
 
@@ -90,13 +97,11 @@ namespace AluraflixAPI.Tests.Controllers
                 Url = "https://www.youtube.com/watch?v=ZXdFisA_hOY"
             };
 
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.ConsultarVideoPorId(1))
+            _mockService.Setup(s => s.ConsultarVideoPorId(1))
                 .Returns(videoConsultado);
 
             // Act
-            var actionResult = controller.ConsultarVideoPorId(2);
+            var actionResult = _controller.ConsultarVideoPorId(2);
             var objectResult = actionResult as NotFoundObjectResult;
 
             // Assert
@@ -105,7 +110,7 @@ namespace AluraflixAPI.Tests.Controllers
         }
 
         [Fact]
-        public void ConsultarVideos_ComStringValida_ResultadoOkComRetorno()
+        public void ConsultarVideos_ComStringValidaENaoPaginado_ResultadoOkComRetorno()
         {
             // Arrange
             List<ReadVideoViewModel> colecaoDeVideos = new List<ReadVideoViewModel>
@@ -128,13 +133,48 @@ namespace AluraflixAPI.Tests.Controllers
                 }
             };
 
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.ConsultarVideos("C#", 0))
+            _mockService.Setup(s => s.ConsultarVideos("C#", 0))
                 .Returns(colecaoDeVideos);
 
             // Act
-            var actionResult = controller.ConsultarVideos("C#");
+            var actionResult = _controller.ConsultarVideos("C#", 0);
+            var objectResult = actionResult as OkObjectResult;
+            var videoResult = objectResult.Value as List<ReadVideoViewModel>;
+
+            // Assert
+            Assert.NotNull(videoResult);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public void ConsultarVideosFree_Maximo5Videos_ResultadoOkComRetorno()
+        {
+            // Arrange
+            List<ReadVideoViewModel> colecaoDeVideos = new List<ReadVideoViewModel>
+            {
+                new ReadVideoViewModel
+                {
+                    Id = 1,
+                    Titulo = ".NET 5 REST API Tutorial C#",
+                    Descricao = "Learn how to create a REST API end-to-end from scratch using the latest " +
+                    ".NET 5 innovations and Visual Studio Code. The API will be written in C#.",
+                    Url = "https://www.youtube.com/watch?v=ZXdFisA_hOY"
+                },
+                new ReadVideoViewModel
+                {
+                    Id = 2,
+                    Titulo = "10 C# Libraries To Save You Time And Energy",
+                    Descricao = "These are 10 C# libraries that I use to make my job simpler. " +
+                    "Learn how to save yourself time and energy from these libraries.",
+                    Url = "https://www.youtube.com/watch?v=uS0hRJqamfU"
+                }
+            };
+
+            _mockService.Setup(s => s.ConsultarVideos(null, 1))
+                .Returns(colecaoDeVideos);
+
+            // Act
+            var actionResult = _controller.ConsultarVideos(null, 1);
             var objectResult = actionResult as OkObjectResult;
             var videoResult = objectResult.Value as List<ReadVideoViewModel>;
 
@@ -147,13 +187,11 @@ namespace AluraflixAPI.Tests.Controllers
         public void RemoverVideoPorId_ComIdValido_ResultadoOkSemRetorno()
         {
             // Arrange
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.RemoverVideoPorId(1))
+            _mockService.Setup(s => s.RemoverVideoPorId(1))
                 .Returns(Result.Ok);
 
             // Act
-            var actionResult = controller.RemoverVideoPorId(1);
+            var actionResult = _controller.RemoverVideoPorId(1);
             var objectResult = actionResult as OkObjectResult;
 
             // Assert
@@ -180,13 +218,12 @@ namespace AluraflixAPI.Tests.Controllers
                     ".NET 5 innovations and Visual Studio Code. The API will be written in C#.",
                 Url = "https://www.youtube.com/watch?v=ZXdFisA_hOY"
             };
-            var mockService = new Mock<IVideoService>();
-            var controller = new VideosController(mockService.Object);
-            mockService.Setup(s => s.AtualizarVideoPorId(1, videoComDadosNovos))
+
+            _mockService.Setup(s => s.AtualizarVideoPorId(1, videoComDadosNovos))
                 .Returns(videoAtualizado);
 
             // Act
-            var actionResult = controller.AtualizarVideoPorId(1, videoComDadosNovos);
+            var actionResult = _controller.AtualizarVideoPorId(1, videoComDadosNovos);
             var objectResult = actionResult as OkObjectResult;
             var videoResult = objectResult.Value as ReadVideoViewModel;
 
